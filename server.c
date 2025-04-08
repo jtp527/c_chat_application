5e814bf8a6add6602e92597d3afeacd51bcf2a69
M9xha <hesap2k13@gmail.com>
2025-04-08 12:31:13 -0400

    added multiple user capability and uses mutex to combat race condition

--- server.c
+++ server.c
@@ -6,6 +6,20 @@
 #include <netinet/in.h>
 
 #define PORT 12345
+#define MAX_CLIENTS 10
+
+int clients[MAX_CLIENTS];
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void broadcast(const char *msg, int sender_socket) {
+    pthread_mutex_lock(&lock);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] != 0 && clients[i] != sender_socket) {
+            send(clients[i], msg, strlen(msg), 0);
+        }
+    }
+    pthread_mutex_unlock(&lock);
+}
 
 void *handle_client(void *arg) {
     int client_socket = *((int *)arg);
@@ -14,16 +28,24 @@ void *handle_client(void *arg) {
     char buffer[1024];
     while (1) {
         int bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-        if (bytes <= 0) {
-            printf("Client disconnected.\n");
-            break;
-        }
+        if (bytes <= 0) break;
         buffer[bytes] = '\0';
         printf("Client says: %s", buffer);
-        send(client_socket, buffer, bytes, 0); // Echo back
+        broadcast(buffer, client_socket);  // Send to other clients
     }
 
     close(client_socket);
+
+    // Remove from client list
+    pthread_mutex_lock(&lock);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i] == client_socket) {
+            clients[i] = 0;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&lock);
+
     return NULL;
 }
 
@@ -44,7 +66,24 @@ int main() {
         socklen_t addr_len = sizeof(client_addr);
         int *client_socket = malloc(sizeof(int));
         *client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
-        printf("Client connected.\n");
+
+        pthread_mutex_lock(&lock);
+        int added = 0;
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i] == 0) {
+                clients[i] = *client_socket;
+                added = 1;
+                break;
+            }
+        }
+        pthread_mutex_unlock(&lock);
+
+        if (!added) {
+            printf("Too many clients.\n");
+            close(*client_socket);
+            free(client_socket);
+            continue;
+        }
 
         pthread_t tid;
         pthread_create(&tid, NULL, handle_client, client_socket);
